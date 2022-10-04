2d95ecd23ed8fbdceaf2674561ed963c352a888a
unknown <you@example.com>
2022-10-04 18:51:54 +0300

    HomeWork_1 is done

---
+++ Task_1/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+//Задача 1. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+/*Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 > number2)
+    {
+        Console.WriteLine($"MaxNumber = {number1}");
+    }
+    else
+    {
+        Console.WriteLine($"MinNumber = {number1}");
+    } 
+    */
+
+//Задача 2. Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+/*Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 == number2 && number1 == number3)
+    {
+        Console.Write("All numbers are equal enter others");
+    }
+    else 
+    {
+        int max = number1;
+            if(number2 > max) max = number2;
+            if(number3 > max) max = number3;
+            Console.WriteLine($"MaxNumber = {max}");
+    }*/
+
+//Задача 3. Напишите программу, которая на вход принимает число и выдает, является ли число четным.
+
+/* Console.Write("Input a Number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a % 2 == 0)
+    {
+        Console.Write($"Number {a} - EvenNumber");
+    } 
+    else
+    {
+        Console.Write($"Number {a} - OddNumber");
+    }*/
+
+//Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+/*Console.Write("Input a number greater than 1: ");
+
+  int N = Convert.ToInt32(Console.ReadLine());
+while ( N > 0)
+{
+    if (N % 2 == 0)
+    {
+        Console.Write(" " + N );
+    }
+    N --;
+}
+Console.Write(" even numbers ");*/
+      
