882633491bd3984cd952281a20af35113c7ed679
unknown <you@example.com>
2022-10-06 15:14:17 +0300

    added task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 1. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+/*Console.Write("Input three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ if (n / 100 > 0)
+ {
+    int averageDigit = n % 100 / 10;
+    Console.WriteLine($"Average digit of {n} is {averageDigit}");
+ }
+ else 
+{
+    Console.WriteLine("Uncorrent input!");
+}*/
+
+//Задача 2. Напишие программу, которая выводит третью цифру заданого числа или сообщает, что третьей цифры нет.
+
+/*Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n /100 >0)
+{
+    int lastDigit = n % 10;
+    Console.WriteLine($"Last Digit of {n} is {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Last Digit is absent. Input another number");
+}*/
+
+//Задача 3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли он выходным.
+
+/*Console.WriteLine("Input the number corresponding to the day of the week: ");
+int n =  Convert.ToInt32(Console.ReadLine());
+
+if (n <= 5)
+{
+    Console.WriteLine($"Weekday");
+}
+else
+{
+    Console.WriteLine("Weekend");
+}*/
