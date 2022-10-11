40ecd8754682b1c03cff64cb0ceaa5425b64250c
unknown <you@example.com>
2022-10-11 12:27:11 +0300

    added task_3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -21,15 +21,46 @@ Console.ReadLine();*/
 
 //Задача 2. Напишите прорамму, которая принимает на вход координаты двух точек и находит расстояние между ними в 3Д пространстве.
 
-/*double FindDistance(double ax, double ay, double az, double bx, double by, double bz);
+/*double FindDistance(double ax, double ay, double az, double bx, double by, double bz)
+{
+    double distance = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+    return distance;
+}
+Console.Write("enter the coordinate x point A: ");
+double ax = Convert.ToDouble(Console.ReadLine());
 
-return Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
-*/
+Console.Write("enter the coordinate y point A: ");
+double ay = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("enter the coordinate z point A: ");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"point A will have coordinates ({ax}, {ay}, {az}).");
+
+Console.Write("enter the coordinate x point B: ");
+double bx = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("enter the coordinate y point B: ");
+double by = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("enter the coordinate z point B: ");
+double bz = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"point B will have coordinates ({bx}, {by}, {bz}).");
+
+double distance = FindDistance(ax, ay, az, bx, by, bz);
+double Distance = Math.Round(distance,2);
+
+Console.WriteLine($"Distance between point A({ax}, {ay}, {az}) and point B({bx}, {by}, {bz}) equally {Distance}");
+
+Console.ReadLine();
+
+
+
+
 
 //Задача 3. Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
 
 
- void CubesOfNumbersA(int n)
+ /*void CubesOfNumbersA(int n)
  {
     int count = 1;
     while(count <= n)
@@ -42,4 +73,5 @@ return Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz,
  Console.WriteLine("Input a number");
  int n = Convert.ToInt32(Console.ReadLine());
  CubesOfNumbersA (n);
- Console.ReadLine();
+ Console.ReadLine();*/
+
